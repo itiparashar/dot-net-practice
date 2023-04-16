119bb4860ca21f309f8af6122719acd2f9227902
iti parashar <sharmaiti175@gmail.com>
2023-04-16 19:08:45 +0530

    first commit

---
+++ print_content_of_file/Program.cs
@@ -0,0 +1,41 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+// Program to print the contents of a file
+// file name must be taken from cooamd line argument
+// Let file will be of txt format
+
+// check args are there
+if(args.Length > 0)
+{
+    for(int i= 0; i<args.Length; i++)
+    {
+        printContents(args[i]);
+    }
+}
+
+void printContents(string filePath)
+{
+    // 1. import necessary library "using system.IO"
+    // 2. check file exists or not
+    // 3. Open the file in read mode
+    // 4. read contents
+    // 5. close the file
+
+    // C# streamreader => to read a file, streamwrite => write the file
+    
+    if (File.Exists(filePath))
+    {
+        // true means file exists
+        StreamReader sr = new StreamReader(filePath);
+        string data = sr.ReadToEnd();
+        sr.Close();
+        Console.WriteLine(data);
+    }
+    else
+    {
+        // false doesnot exists
+        Console.WriteLine("File does not exists");
+    }
+}
+
+
